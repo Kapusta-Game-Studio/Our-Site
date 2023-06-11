ece571dbcc5bef69a9028e720885444766087fc3
Justhack <nedikov.misha@yandex.ru>
2023-06-11 23:27:53 +0400

    8-INTERNET

--- custom-script.js
+++
@@ -1,9 +0,0 @@
-
-window.onresize = function curusel() {
-     if( window.innerWidth <= 576 ) {
-           document.getElementById('curusel-custom').style.display = 'flex';
-           document.getElementById('tub-col-item-custom').style.display = 'none';
-      } else {
-           document.getElementById('curusel-custom').style.display = 'none';
-           document.getElementById('tub-col-item-custom').style.display = 'flex';
-     }}
